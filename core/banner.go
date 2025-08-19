a824778946af146fe6e3acd83f3cc58892759dc3
Evil-twist <izlashcompany@gmail.com>
2025-08-18 17:17:34 -0700

    Update banner.go

--- core/banner.go
+++ core/banner.go
@@ -8,50 +8,49 @@ import (
 )
 
 const (
-	VERSION = "3.8.0"
+	VERSION = "4.1.0"
 )
 
 func putAsciiArt(s string) {
 	for _, c := range s {
 		d := string(c)
-		switch string(c) {
-		case " ":
-			color.Set(color.BgBlue)
+		switch {
+		case d == " ":
+			color.Set(color.FgBlack) // Default to black for spaces
 			d = " "
-		case "@":
+		case strings.Contains("☠", d): // Border elements in yellow
+			color.Set(color.FgMagenta)
+		case d == ":" || d == ":": // ":" or ":" in red
+			color.Set(color.FgRed)
+		case d == "!" || d == "!": // "!" or "!" in blue
+			color.Set(color.FgRed)
+		case d == "@": // Specific character "@" in red
+			color.Set(color.FgRed)	
+		case d == "\n": // Reset color on newline
 			color.Unset()
-			d = " "
-		case "#":
-			color.Set(color.BgRed)
-			d = " "
-		case "W":
-			color.Set(color.BgWhite)
-			d = " "
-		case "_":
-			color.Unset()
-			d = " "
-		case "║":
-            color.Set(color.FgWhite)
-            d= " "
-        case "═":
-            color.Set(color.FgWhite)		
-		case "\n":
-			color.Unset()
+		case strings.Contains("EXODUS", d): // PRO VERSION text in yellow
+			color.Set(color.FgMagenta)
+        case strings.Contains("v4.1.0", d): // VERSION number in green
+			color.Set(color.FgGreen)
+		case strings.Contains("Buy on https://www.clirpa.com", d):
+			color.Set(color.FgGreen)
+		default:
+			color.Set(color.FgHiBlack) // Default all other characters to black
 		}
 		fmt.Print(d)
 	}
 	color.Unset()
 }
 
+
 func printLogo(s string) {
 	for _, c := range s {
 		d := string(c)
-		switch string(c) {
+		switch d {
 		case "_":
-			color.Set(color.FgWhite)
-		case "@":
+			color.Set(color.FgYellow) // Change color to yellow
+		case "\n":
 			color.Unset()
-			d = " "
 		default:
 			color.Set(color.FgHiBlack)
 		}
@@ -60,61 +59,25 @@ func printLogo(s string) {
 	color.Unset()
 }
 
-func printUpdateName() {
-	nameClr := color.New(color.FgMagenta)
-	txt := nameClr.Sprintf("           ⥺   ↭↭↭↭↭ Eviltwist (Evilginx mod) ↭↭↭↭↭  ⭄  ")
-	fmt.Fprintf(color.Output, "%s", txt)
-}
-
-func printOneliner1() {
-	handleClr := color.New(color.FgHiBlue)
-	versionClr := color.New(color.FgMagenta)
-	textClr := color.New(color.FgHiBlack)
-	spc := strings.Repeat(" ", 10-len(VERSION))
-	txt := textClr.Sprintf("      modified by Young Boss (") + handleClr.Sprintf("@Eviltwist") + textClr.Sprintf(")") + spc + textClr.Sprintf("version ") + versionClr.Sprintf("%s", VERSION)
-	fmt.Fprintf(color.Output, "%s", txt)
-}
-
-func printOneliner2() {
-	textClr := color.New(color.FgHiBlack)
-	red := color.New(color.FgRed)
-	white := color.New(color.FgWhite)
-	txt := textClr.Sprintf("                   NO ") + red.Sprintf("PEACE") + white.Sprintf(" - ") + textClr.Sprintf("DO ") + red.Sprintf("EVIL")
-	fmt.Fprintf(color.Output, "%s", txt)
-}
 
 func Banner() {
 	fmt.Println()
-	
-	putAsciiArt("__                                     __\n")
-	putAsciiArt("_   @@     @@@@@@@@@@@@@@@@@@@     @@   _")
-	printLogo("    ######## ###  ### ### ###      ####### ###  ###  ### ###  ###### #######")
-	fmt.Println()
-	putAsciiArt("  @@@@    @@@@@@@@@@@@@@@@@@@@@    @@@@  ")
-	printLogo("    ##!      ##!  ### ##! ##!        ##!   ##!  ##!  ##! ##! !##       ##!")
-	fmt.Println()
-	putAsciiArt("  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  ")
-	printLogo("    #!!!:!   #!#  !#! !!# #!!        #!!   #!!  !!#  #!# !!#  !##!!    #!!")
-	fmt.Println()
-	putAsciiArt("   @@@@@@@@@@###@@@@@@@###@@@@@@@@@@     ")
-	printLogo("    !!:       !: .:!  !!: !!:        !!:    !:  !!:  !!  !!:     !:!   !!:")
-	fmt.Println()
-	putAsciiArt("     @@@@@@@#####@@@@@#####@@@@@@@       ")
-	printLogo("    :!:!!:::   ::::   ::! !!:::.:::  !!:      ::.: ::::  ::!  ::.: :   ::!")
-	fmt.Println()
-	putAsciiArt("      @@@@@@@###@@@@@@@###@@@@@@@        ")
-	printLogo("    :!:!!:::    :!    ::! !!:::.:::   !:       !:   ::   !!:  !!:::.   !!:")
-	fmt.Println()
-	putAsciiArt("      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@      \n")
-	putAsciiArt("     @@@@@WW@@@WW@@WWW@@WW@@@WW@@@@@     ")
-	printUpdateName()
-	fmt.Println()
-	putAsciiArt("    @@@@@@WW@@@WW@@WWW@@WW@@@WW@@@@@@    \n")
-	//printOneliner2()
-	//fmt.Println()
-	putAsciiArt("_   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   _")
-	printOneliner1()
-	fmt.Println()
-	putAsciiArt("__                                     __\n")
-	fmt.Println()
-     }
+
+	// Display the new ASCII art banner with yellow border and red content
+	putAsciiArt(`
+☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠
+☠     @@@@@@@@  @@@  @@@   @@@@@@   @@@@@@@   @@@  @@@   @@@@@@         ☠
+☠     @@@@@@@@  @@@  @@@  @@@@@@@@  @@@@@@@@  @@@  @@@  @@@@@@@         ☠
+☠     @@!       @@!  !@@  @@!  @@@  @@!  @@@  @@!  @@@  !@@             ☠
+☠     !@!       !@!  @!!  !@!  @!@  !@!  @!@  !@!  @!@  !@!             ☠
+☠     @!!!:!     !@@!@!   @!@  !@!  @!@  !@!  @!@  !@!  !!@@!!          ☠
+☠     !!!!!:      @!!!    !@!  !!!  !@!  !!!  !@!  !!!   !!@!!!         ☠
+☠     !!:        !: :!!   !!:  !!!  !!:  !!!  !!:  !!!       !:!        ☠
+☠     :!:       :!:  !:!  :!:  !:!  :!:  !:!  :!:  !:!      !:!         ☠
+☠     !:: ::::  !::  :::  ::::: ::  !:::: ::  :::::!::  ::::!::         ☠
+☠     : :: ::   !:   ::    : :  :   :: :  :    : :  :   :: : :          ☠
+☠     Buy on https://www.clirpa.com                   EXODUS v4.1.0     ☠
+☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠ ☠
+`)
+
+}
